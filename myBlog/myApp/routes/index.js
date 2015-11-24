ea305e19d273e85563369dc117a3a7d57b9c69fb
WallXue <good_xw@qq.com>
2015-11-24 23:42:10 +0800

    编码问题未解决

--- myBlog/myApp/routes/index.js
+++ myBlog/myApp/routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
+var myZone = require('../service/myZone');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	console.log(path.posix.basename());
-  	res.sendfile('E:/myApp/views/index.html');
+  	res.sendfile('C:/gitbase/myBlog/myApp/views/index.html');
 });
 
+
+router.get('/test', function(req, res, next) {
+	console.log(myZone);
+	myZone.test();
+});
 module.exports = router;
